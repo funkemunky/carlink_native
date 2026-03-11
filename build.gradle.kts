66dad9d7cceb42b440768286d102f516482ba6bd
Dawson <dawson@funkemunky.cc>
2026-03-11 10:00:38 -0400

    Downgrading to 8.12.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file for Carlink Native Android App
 
 plugins {
-    id("com.android.application") version "9.0.0" apply false
+    id("com.android.application") version "8.12.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
 }
